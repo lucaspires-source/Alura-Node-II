5e48001477470f3b4e754b691de3940080a3e13f
lucaspires-source <pires.lucas94@gmail.com>
2021-04-21 12:11:19 -0300

    Aula 01.03 Entendendo a API

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const config = require('config')
+app.use(bodyParser.json())
+app.listen(config.get('api.porta'), () => console.log('App is running'))
