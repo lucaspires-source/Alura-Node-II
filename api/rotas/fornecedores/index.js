1f0138b2bf29d12993abd58e607ded88b54b6557
lucaspires-source <pires.lucas94@gmail.com>
2021-05-01 10:14:27 -0300

    Aula 03.03 Declarando Sucesso na resposta

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -3,6 +3,7 @@ const TabelaFornecedor = require("./TabelaFornecedor");
 const Fornecedor = require("./Fornecedor");
 roteador.get("/", async (req, res) => {
   const resultados = await TabelaFornecedor.listar();
+  res.status(200)
   res.send(JSON.stringify(resultados));
 });
 
@@ -12,6 +13,7 @@ roteador.post("/", async (req, res) => {
     const dadosRecebidos = req.body;
     const fornecedor = new Fornecedor(dadosRecebidos);
     await fornecedor.criar();
+    res.status(201)
     res.send(JSON.stringify(fornecedor));
   } catch (erro) {
     res.send(
@@ -27,6 +29,7 @@ roteador.get("/:idFornecedor", async (req, res) => {
     const id = req.params.idFornecedor;
     const fornecedor = new Fornecedor({ id: id });
     await fornecedor.carregar();
+    res.status(200)
     res.send(JSON.stringify(fornecedor));
   } catch (erro) {
     res.send(
@@ -45,7 +48,7 @@ roteador.put("/:idFornecedor", async(req,res) =>{
     const dados= Object.assign({}, dadosRecebidos,{id:id})
     const fornecedor = new Fornecedor(dados)
     await fornecedor.atualizar()
-  
+    res.status(204)
     res.end()
   }catch(erro){
     res.send(
@@ -64,6 +67,7 @@ roteador.delete('/:idFornecedor', async(req,res) =>{
     const fornecedor = new Fornecedor({id:id})
     await  fornecedor.carregar()
     await fornecedor.remover()
+    res.status(204)
     res.end()
   }catch(erro){
     res.send(
