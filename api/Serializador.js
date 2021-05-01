f7a87e2163851fcbdee3f4ecb8dc8c64809d4e8c
lucaspires-source <pires.lucas94@gmail.com>
2021-05-01 17:21:18 -0300

    Aula 06.04 Processando XML no serializador

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,58 +1,78 @@
-const ValorNaoSuportado = require("./erros/ValorNaoSuportado")
-
-class Serializador{
-    json(dados){
-        return JSON.stringify(dados)
-    }
-    serializar(dados){
-        if (this.contentType === 'application/json'){
-            return this.json(this.filtrar(dados))
-        }
-        else{
-            throw new ValorNaoSuportado(this.contentType)
-        }
-    }
-    filtrarObjeto(dados){
-        const novoObjeto = {}
-
-        this.camposPublicos.forEach((campo) =>{
-            if (dados.hasOwnProperty(campo)){
-                novoObjeto[campo]
+const ValorNaoSuportado = require("./erros/ValorNaoSuportado");
+const jsontoxml = require("jsontoxml");
+class Serializador {
+  json(dados) {
+    return JSON.stringify(dados);
+  }
+  xml(dados) {
+    let tag = this.tagSingular
+    if(Array.isArray(dados)){
+        tag = this.tagPlural
+        dados =dados.map((item)=>{
+            return{
+                [this.tagSingular]:item
             }
         })
-        return novoObjeto
     }
-    filtrar(dados){
-        if(Array.isArray(dados)){
-            dados=dados.map(this.filtrarObjeto)
-        }else{
-            dados = this.filtrarObjeto(dados)
-        }
-        return dados
+    return jsontoxml({ [tag]: dados });
+  }
+  serializar(dados) {
+    dados = this.filtrar(dados);
+    if (this.contentType === "application/json") {
+      return this.json(dados);
     }
+
+    if (this.contentType === "application/xml") {
+      return this.xml(dados);
+    } else {
+      throw new ValorNaoSuportado(this.contentType);
+    }
+  }
+  filtrarObjeto(dados) {
+    const novoObjeto = {};
+
+    this.camposPublicos.forEach((campo) => {
+      if (dados.hasOwnProperty(campo)) {
+        novoObjeto[campo];
+      }
+    });
+    return novoObjeto;
+  }
+  filtrar(dados) {
+    if (Array.isArray(dados)) {
+      dados = dados.map(this.filtrarObjeto);
+    } else {
+      dados = this.filtrarObjeto(dados);
+    }
+    return dados;
+  }
 }
 
-class SerelizadorFornecedor extends Serializador{
-    constructor(contentType, camposExtras){
-        super()
-        this.contentType = contentType
-        this.camposPublicos = ['id','empresa','categoria'].concat(camposExtras|| [])
+class SerelizadorFornecedor extends Serializador {
+  constructor(contentType, camposExtras) {
+    super();
+    this.contentType = contentType;
+    this.camposPublicos = ["id", "empresa", "categoria"].concat(
+      camposExtras || []
+    );
 
-    }
+    this.tagSingular =  "fornecedor";
+    this.tagPlural="fornecedores"
+  }
 }
 
-class SerelizadorErro extends Serializador{
-    constructor(contentType,camposExtras){
-        super()
-        this.contentType = contentType
-        this.camposPublicos = ['id','mensagem'].concat(camposExtras|| [])
-    }
-
+class SerelizadorErro extends Serializador {
+  constructor(contentType, camposExtras) {
+    super();
+    this.contentType = contentType;
+    this.camposPublicos = ["id", "mensagem"].concat(camposExtras || []);
+    this.tagSingular = "erro";
+    this.tagPlural ="erros"
+  }
 }
 module.exports = {
-    Serializador:Serializador,
-    formatosAceitos:['application/json'],
-    SerelizadorFornecedor:SerelizadorFornecedor,
-    SerelizadorErro:SerelizadorErro
-
-}
+  Serializador: Serializador,
+  formatosAceitos: ["application/json", "application/xml"],
+  SerelizadorFornecedor: SerelizadorFornecedor,
+  SerelizadorErro: SerelizadorErro,
+};
