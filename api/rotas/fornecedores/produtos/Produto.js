45b156e9d51c517c78ca604eab4df9fb23e3bd6c
lucaspires-source <pires.lucas94@gmail.com>
2021-05-08 13:28:38 -0300

    Aula 02.02 Declarando a rota POST

---
+++ api/rotas/fornecedores/produtos/Produto.js
@@ -0,0 +1,28 @@
+class Produto{
+constructor({id,titulo,preco,estoque,fornecedor,dataCriacao,dataAtualização,versao}){
+    this.id = id;
+    this.titulo = titulo;
+    this.preco = preco;
+    this.estoque = estoque;
+    this.fornecedor = fornecedor;
+    this.dataCriacao = dataCriacao;
+    this.dataAtualizacao = dataAtualizacao;
+    this.versao = versao;
+}
+
+async criar() {
+    this.validar()
+    const resultado = await Tabela.inserir({
+      titulo: this.titulo,
+      preco: this.preco,
+      estoque: this.estoque,
+      fornecedor:this.fornecedor 
+    });
+    this.id = resultado.id;
+    this.dataCriacao = resultado.dataCriacao;
+    this.dataAtualizacao = resultado.dataAtualizacao;
+    this.versao = resultado.versao;
+  }
+}
+
+module.exports = Produto
--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,9 +1,17 @@
 const roteador = require("express").Router({ mergeParams: true });
 const Tabela = require("./TabelaProduto");
-
+const Produto = require("./Produto")
 roteador.get("/", async (req, res) => {
   const produtos = await Tabela.listar(req.params.idFornecedor);
   res.send(JSON.stringify([]));
 });
 
+
+roteador.post('/', async (req,res) =>{
+    const idFornecedor = req.params.idFornecedor
+    const body = req.body
+    const dados = Object.assign({},body,{fornecedor: idFornecedor})
+    const produto = new Produto(dados)
+    await produto.criar()
+})
 module.exports = roteador;
